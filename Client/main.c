8287548c9a29aa1c6b5881ac2ac6bce19a7fd3c5
Tomislav Celic <celer.tomislav@gmail.com>
2025-01-11 16:25:09 +0100

    Add LV4 Server and Client

---
+++ Client/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <Windows.h>
+#include <tchar.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define COMMUNICATION_BUFFER 256
+
+INT32 _tmain(INT32 argc, LPTSTR argv[])
+{
+	INT32 returnValue = 0;
+	if (argc != 4)
+	{
+		_tprintf(_T("Error. Incorrect param number. Param number must be 3.\nUsage: client [NAME] [IP] [PORT])"));
+		returnValue = 1;
+		goto exit;
+	}
+
+	LPTSTR IpAddress = argv[2];
+	UINT16 port = _ttoi(argv[3]);
+	LPTSTR name = argv[1];
+	UINT32 nameLength = _tcslen(name);
+
+	WSADATA data;
+	if (WSAStartup(MAKEWORD(2, 2), &data) != 0)
+	{
+		_tprintf(_T("Cannot start Winsock. Error: %d"), WSAGetLastError());
+		returnValue = 2;
+		goto exit;
+	}
+
+	SOCKET communicationSocket;
+	SOCKADDR_IN socketAddress;
+
+	communicationSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	if (communicationSocket == INVALID_SOCKET)
+	{
+		_tprintf(_T("Cannot create socket. Error: %d"), WSAGetLastError());
+		returnValue = 3;
+		goto exit;
+	}
+
+	socketAddress.sin_family = AF_INET;
+	socketAddress.sin_port = htons(port);
+	if (InetPton(AF_INET, IpAddress, &socketAddress.sin_addr) != 1)
+	{
+		_tprintf(_T("Invalid IP adress format. Error: %d"), WSAGetLastError());
+		returnValue = 4;
+		goto exit;
+	}
+
+	if (connect(communicationSocket, (SOCKADDR*)&socketAddress, sizeof(socketAddress)) == SOCKET_ERROR)
+	{
+		_tprintf(_T("Cannot connect. Error: %d"), WSAGetLastError());
+		returnValue = 5;
+		goto exit;
+	}
+
+	TCHAR* messageBuffer[COMMUNICATION_BUFFER] = { 0 };
+	_stprintf_s(messageBuffer, sizeof(messageBuffer) / sizeof(TCHAR), _T("N%s"), name);
+	send(communicationSocket, messageBuffer, nameLength * sizeof(TCHAR) + sizeof(TCHAR), 0);
+
+	TCHAR* replyBuffer[COMMUNICATION_BUFFER] = { 0 };
+	UINT32 bytesRecieved = 0;
+
+	if ((bytesRecieved = recv(communicationSocket, replyBuffer, sizeof(replyBuffer) / sizeof(TCHAR), 0)) > 0)
+	{
+		_tprintf(_T("Server responded with a message: %s\n"), replyBuffer);
+		bytesRecieved = 0;
+	}
+
+	if ((bytesRecieved = recv(communicationSocket, replyBuffer, sizeof(replyBuffer) / sizeof(TCHAR), 0)) > 0)
+	{
+		_tprintf(_T("Server responded with a message: %s\n"), replyBuffer);
+		send(communicationSocket, _T("OK\0"), 3 * sizeof(TCHAR), 0);
+	}
+
+exit:
+	if (communicationSocket != INVALID_SOCKET)
+		closesocket(communicationSocket);
+	WSACleanup();
+	return returnValue;
+}
---
+++ Server/main.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <Windows.h>
+#include <tchar.h>
+
+#include "errors.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define CLIENT_COUNT 4
+#define COMMUNICATION_BUFFER 256
+
+UINT32 numberOfConnections = 0;
+HANDLE maxClientsReachedEvent;
+
+typedef struct ThreadParam
+{
+	UINT8 clientNumber;
+	SOCKET clientSocket;
+}THREAD_PARAMS;
+
+DWORD WINAPI HandleClientConnection(LPVOID lpParam)
+{
+	THREAD_PARAMS* params = (THREAD_PARAMS*)lpParam;
+	SOCKET clientSocket = params->clientSocket;
+	UINT8 clientNumber = params->clientNumber;
+
+	TCHAR buffer[COMMUNICATION_BUFFER] = { 0 };
+	UINT32 bytesRecieved = 0;
+
+	if ((bytesRecieved = recv(clientSocket, buffer, sizeof(buffer) / sizeof(TCHAR), 0)) > 0)
+	{
+		_tprintf(_T("Client number %d sends the following message: %s\n"), clientNumber + 1, buffer);
+		bytesRecieved = 0;
+
+		TCHAR* reply[3] = { 0 };
+		_stprintf_s(reply, 3 * sizeof(TCHAR), _T("B%d"), clientNumber + 1);
+		send(clientSocket, reply, 3 * sizeof(TCHAR), 0);
+	}
+
+	if ((clientNumber + 1) == CLIENT_COUNT)
+	{
+		SetEvent(maxClientsReachedEvent);
+	}
+
+	WaitForSingleObject(maxClientsReachedEvent, INFINITE);
+
+	send(clientSocket, _T("F\0"), 2 * sizeof(TCHAR), 0);
+	if ((bytesRecieved = recv(clientSocket, buffer, sizeof(buffer) / sizeof(TCHAR), 0)) > 0)
+	{
+		_tprintf(_T("Client number %d sends the following message: %s\n"), clientNumber + 1, buffer);
+		bytesRecieved = 0;
+	}
+
+	return 0;
+}
+
+INT32 _tmain(INT32 argc, LPTSTR argv[])
+{
+	UINT32 returnValue = 0;
+	if (argc != 3)
+	{
+		_tprintf(_T("Error. Incorrect param number. Param number must be 2.\nUsage: server [IP] [PORT]\nExample: server 127.0.0.1 8000\nIf you want to use automatic IP detection write IP adress as negative integer\nExample: server -1 8000\n(runs the first automatically detected IP, for second use -2 etc...)"));
+		return INVALID_ARG_NUMBER;
+	}
+
+	WSADATA data;
+	if (WSAStartup(MAKEWORD(2, 2), &data) != 0)
+	{
+		_tprintf(_T("Cannot start Winsock. Error: %d"), WSAGetLastError());
+		return CANNOT_START_WSA;
+	}
+
+	LPTSTR IpAddress = argv[1];
+
+	UINT16 port = _ttoi(argv[2]);
+	if (_ttoi(argv[1]) < 0)
+	{
+		char hostname[256];
+		if (gethostname(hostname, sizeof(hostname)) == SOCKET_ERROR)
+		{
+			_tprintf(_T("Failed to get hostname. Error: %d\n"), WSAGetLastError());
+			returnValue = CANNOT_GET_HOSTNAME;
+			goto exit;
+		}
+
+		struct hostent* host = gethostbyname(hostname);
+		if (!host)
+		{
+			_tprintf(_T("Failed to get host by name. Error: %d\n"), WSAGetLastError());
+			returnValue = CANNOT_GET_HOST_BY_NAME;
+			goto exit;
+		}
+
+		struct in_addr* addr = (struct in_addr*)host->h_addr_list[_ttoi(argv[1]) * -1];
+		TCHAR buffer[256];
+		_stprintf_s(buffer, sizeof(buffer) / sizeof(TCHAR), _T("%d.%d.%d.%d\0"),
+			(addr->S_un.S_un_b.s_b1),
+			(addr->S_un.S_un_b.s_b2),
+			(addr->S_un.S_un_b.s_b3),
+			(addr->S_un.S_un_b.s_b4)
+		);
+
+		IpAddress = &buffer;
+	}
+
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		_tprintf(_T("Cannot create a listening socket. Error: %d"), WSAGetLastError());
+		returnValue = CANNOT_CREATE_SOCKET;
+		goto exit;
+	}
+
+	SOCKADDR_IN listenSocketAddr = { 0 };
+	listenSocketAddr.sin_family = AF_INET;
+	listenSocketAddr.sin_port = htons(port);
+	if (InetPton(AF_INET, IpAddress, &listenSocketAddr.sin_addr) != 1)
+	{
+		_tprintf(_T("Invalid IP adress format. Error: %d"), WSAGetLastError());
+		returnValue = INVALID_IP_ADDR_FORMAT;
+		goto exit;
+	}
+	
+	if (bind(listenSocket, (SOCKADDR*)&listenSocketAddr, sizeof(listenSocketAddr)) == SOCKET_ERROR)
+	{
+		_tprintf(_T("Listen socket bind failed. Error: %d\n"), WSAGetLastError());
+		returnValue = SOCKET_BIND_FAIL;
+		goto exit;
+	}
+
+	SOCKET clientSockets[CLIENT_COUNT] = { 0 };
+	for (UINT32 i = 0; i < CLIENT_COUNT; i++)
+	{
+		clientSockets[i] = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+		if (clientSockets[i] == INVALID_SOCKET)
+		{
+			_tprintf(_T("Cannot create a client socket. Error: %d"), WSAGetLastError());
+			returnValue = CANNOT_CREATE_SOCKET;
+			goto exit;
+		}
+	}
+
+	maxClientsReachedEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (maxClientsReachedEvent == NULL)
+	{
+		_tprintf(_T("Cannot initialize event for max clients."));
+		returnValue = CANNOT_INIT_MAX_CLIENTS_EVENT;
+		goto exit;
+	}
+
+	listen(listenSocket, CLIENT_COUNT);
+	_tprintf(_T("SERVER STARTED ON: %s:%d\n"), IpAddress, port);
+
+	UINT32 numberOfConnections = 0;
+	HANDLE clientThreadHandles[CLIENT_COUNT] = { 0 };
+	THREAD_PARAMS threadParams[CLIENT_COUNT] = { 0 };
+	for (UINT32 i = 0; i < CLIENT_COUNT;)
+	{
+		clientSockets[i] = accept(listenSocket, NULL, NULL);
+		if (clientSockets[i] == INVALID_SOCKET)
+		{
+			_tprintf(_T("Cannot accept a client socket. Error: %d\n"), WSAGetLastError());
+			continue;
+		}
+
+		threadParams[i].clientNumber = i;
+		threadParams[i].clientSocket = clientSockets[i];
+
+		clientThreadHandles[i] = CreateThread(NULL, 0, HandleClientConnection, (LPVOID)&threadParams[i], 0, NULL);
+		i++;
+	}
+	
+	WaitForMultipleObjects(CLIENT_COUNT, &clientThreadHandles, TRUE, INFINITE);
+
+exit:
+	for (UINT32 i = 0; i < CLIENT_COUNT; i++)
+	{
+		if (clientSockets[i] != INVALID_SOCKET)
+			closesocket(clientSockets[i]);
+	}
+
+	if (listenSocket != INVALID_SOCKET)
+		closesocket(listenSocket);
+
+	if (maxClientsReachedEvent != NULL)
+		CloseHandle(maxClientsReachedEvent);
+
+	WSACleanup();
+	return returnValue;
+}
